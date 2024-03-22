4b72c24bc3175b0541d9caf19b602923446f159e
Himnish-N <126179222+Himnish-N@users.noreply.github.com>
2024-03-22 20:10:48 +0530

    valid files to check the lexe and parser works

---
+++ switch_case.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main()
+{
+    int n = 0;
+    // switch case
+    for (n)
+    {
+    case 0:
+       // n++;
+        break;
+    
+    case 1:
+       // n++;
+        break;
+    
+    default:
+        break;
+    }
+    
+}
